dfaa894789827e7ca5d5ca603d28020d81539e6e
Joonas Vanninen <joonas.vanninen@reaktor.com>
2019-12-27 12:24:17 +0200

    Add commits to sentry release

--- post-build.sh
+++ post-build.sh
@@ -1,1 +1,3 @@
+./node_modules/.bin/sentry-cli releases new $NOW_GITHUB_COMMIT_SHA 
+./node_modules/.bin/sentry-cli releases set-commits --auto $NOW_GITHUB_COMMIT_SHA 
 ./node_modules/.bin/sentry-cli releases files $NOW_GITHUB_COMMIT_SHA upload-sourcemaps .next
