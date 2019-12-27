ca7a359ecc0f337826e979e5bea927855c0b441b
Joonas Vanninen <joonas.vanninen@reaktor.com>
2019-12-27 14:07:00 +0200

    Change url prefixx

--- post-build.sh
+++ post-build.sh
@@ -1,3 +1,3 @@
 ./node_modules/.bin/sentry-cli releases new $NOW_GITHUB_COMMIT_SHA 
 ./node_modules/.bin/sentry-cli releases set-commits --commit $SENTRY_REPO"@"$NOW_GITHUB_COMMIT_SHA $NOW_GITHUB_COMMIT_SHA 
-./node_modules/.bin/sentry-cli releases files $NOW_GITHUB_COMMIT_SHA upload-sourcemaps .next --url-prefix '~/_next' --rewrite
+./node_modules/.bin/sentry-cli releases files $NOW_GITHUB_COMMIT_SHA upload-sourcemaps .next --url-prefix '~/.next' --rewrite
