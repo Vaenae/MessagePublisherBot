1950f661cfefbf1965ea039eec01994a378fdede
Joonas Vanninen <joonas.vanninen@reaktor.com>
2019-12-27 11:51:00 +0200

    Add logging

--- post-build.sh
+++ post-build.sh
@@ -1,1 +1,3 @@
-./node_modules/.bin/sentry-cli releases files $SENTRY_RELEASE upload-sourcemaps .next
+echo $SENTRY_RELEASE
+echo $NOW_GITHUB_COMMIT_SHA
+./node_modules/.bin/sentry-cli releases files $NOW_GITHUB_COMMIT_SHA upload-sourcemaps .next
