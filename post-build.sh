66150fa0c9d95ba10c7424f4980b72afee555db6
Joonas Vanninen <joonas.vanninen@reaktor.com>
2019-12-29 11:59:24 +0200

    Only create release on master

--- post-build.sh
+++ post-build.sh
@@ -1,3 +1,7 @@
-./node_modules/.bin/sentry-cli releases new $NOW_GITHUB_COMMIT_SHA 
-./node_modules/.bin/sentry-cli releases set-commits --commit $SENTRY_REPO"@"$NOW_GITHUB_COMMIT_SHA $NOW_GITHUB_COMMIT_SHA 
-./node_modules/.bin/sentry-cli releases files $NOW_GITHUB_COMMIT_SHA upload-sourcemaps .next --url-prefix '~/_next' --rewrite
+#!/bin/bash
+if [ "$NOW_GITHUB_COMMIT_REF" = "master" ]
+then
+  ./node_modules/.bin/sentry-cli releases new $NOW_GITHUB_COMMIT_SHA 
+  ./node_modules/.bin/sentry-cli releases set-commits --commit $SENTRY_REPO"@"$NOW_GITHUB_COMMIT_SHA $NOW_GITHUB_COMMIT_SHA 
+  ./node_modules/.bin/sentry-cli releases files $NOW_GITHUB_COMMIT_SHA upload-sourcemaps .next --url-prefix '~/_next' --rewrite
+fi
