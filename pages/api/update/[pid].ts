873f9b5f5944bcf70452c2cf777596161cb1e8d3
Vaenae <jvannine@gmail.com>
2020-01-23 21:09:22 +0200

    Respond with 200

--- pages/api/update/[pid].ts
+++ pages/api/update/[pid].ts
@@ -23,5 +23,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         return
     }
     await bot.handleUpdate(req.body, res)
+    res.status(200).send('k')
     return
 }
