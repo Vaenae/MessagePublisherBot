a2a5c157436da99d437f8df94b6eb2d8a6ab8252
Vaenae <jvannine@gmail.com>
2020-01-23 19:53:35 +0200

    Add logging to webhook registration

--- scripts/register-webhook.ts
+++ scripts/register-webhook.ts
@@ -5,4 +5,10 @@ const token = process.env.BOT_TOKEN!
 const webhookUrl = `${baseUrl}/api/update/${token}`
 const bot = new Telegraf(token)
 
-bot.telegram.setWebhook(webhookUrl)
+console.log(`Registering webhook with url ${webhookUrl}`)
+bot.telegram
+    .setWebhook(webhookUrl)
+    .then(result => console.log(`Done registering webhook, result ${result}`))
+    .catch(reason =>
+        console.error(`Failed registering webhook, reason ${reason}`)
+    )
