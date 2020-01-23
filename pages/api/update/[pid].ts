d94e9c92fc8dc039be1762edf2452f907036fb06
Vaenae <jvannine@gmail.com>
2020-01-23 20:13:02 +0200

    Use correct route, return status

---
+++ pages/api/update/[pid].ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { serverConfig } from '../../../config/config'
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    const {
+        query: { pid },
+        method
+    } = req
+    if (pid !== serverConfig.botToken) {
+        console.error(`Got message with wrong pid ${pid}`)
+        res.status(404)
+        return
+    }
+    if (method !== 'POST') {
+        console.error(`Got message with method ${method} ${pid}`)
+        res.status(404)
+        return
+    }
+    console.log(`Got message with pid ${pid}`)
+    console.log(req.body)
+    res.status(200)
+}
