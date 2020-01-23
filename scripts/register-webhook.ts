e8f4908095be4acf85fece9eaf2c34924155b50a
Vaenae <jvannine@gmail.com>
2020-01-23 20:42:35 +0200

    Show status on register

--- scripts/register-webhook.ts
+++ scripts/register-webhook.ts
@@ -5,10 +5,15 @@ const token = process.env.BOT_TOKEN!
 const webhookUrl = `${baseUrl}/api/update/${token}`
 const bot = new Telegraf(token)
 
-console.log(`Registering webhook with url ${webhookUrl}`)
-bot.telegram
-    .setWebhook(webhookUrl)
-    .then(result => console.log(`Done registering webhook, result ${result}`))
-    .catch(reason =>
+async function registerWebhook() {
+    try {
+        const webhookInfo = await bot.telegram.getWebhookInfo()
+        console.log(`Current webhook info: ${JSON.stringify(webhookInfo)}`)
+        const result = await bot.telegram.setWebhook(webhookUrl)
+        console.log(`Done registering webhook, result ${result}`)
+    } catch (reason) {
         console.error(`Failed registering webhook, reason ${reason}`)
-    )
+    }
+}
+
+registerWebhook()
